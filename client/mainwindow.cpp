f43f385b59380f4959f8b383ef205647f3193741
ardalan-di <ardalandelfardi@gmail.com>
2022-06-26 12:11:48 +0430

    chatbar

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -6,10 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->test,&QPushButton::clicked,this,&MainWindow::add_safebar);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::add_safebar()
+{
+    QVBoxLayout* layout = qobject_cast<QVBoxLayout *>(ui->verticalLayout->layout());
+
+    QPushButton *Button = new QPushButton("chat",ui->groupBox_3);
+    Button->setStyleSheet("border: 1px solid; border-radius:10px; background-color: palette(base); ");
+    layout->insertWidget(0,Button);
+
+
+}
+void MainWindow::on_test_clicked()
+{
+
 }
 
